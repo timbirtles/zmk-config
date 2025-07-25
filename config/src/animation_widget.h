c06da3e4f4c583866dd9e20e892d7080275f736b
Tim Birtles <timbirtles2004@gmail.com>
2025-07-25 21:25:12 +0100

    OLEd test v3

--- config/src/animation_widget.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <lvgl.h>
-
-int animation_widget_init(lv_obj_t *parent);
--- config/src/display.c
+++
@@ -1,7 +0,0 @@
-#include <zmk/display.h>
-#include <lvgl.h>
-#include "display/animation_widget.h"
-
-void zmk_display_app_init(lv_obj_t *parent) {
-    animation_widget_init(parent);
-}
